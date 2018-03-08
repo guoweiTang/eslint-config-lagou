f8d36389f485546076c241f23be3fc4cc4da58c9
Ice <ice@lagou.com>
2018-03-08 17:52:54 +0800

    init

---
+++ index.js
@@ -0,0 +1,163 @@
+/**
+ * @fileoverview Used for exporting ESLint Configuring.
+ * @author ice.tang
+ */
+'use strict';
+module.exports = {
+    "rules": {
+        "indent": [
+            1,
+            4
+        ],
+        "semi-spacing": [
+            1
+        ],
+        "comma-spacing": [
+            1
+        ],
+        "space-before-blocks": [
+            1,
+            "always"
+        ],
+        "space-before-function-paren": [
+            1,
+            "never"
+        ],
+        "space-infix-ops": [
+            1
+        ],
+        "space-in-parens": [
+            1
+        ],
+        "no-multi-spaces": [
+            1
+        ],
+        "keyword-spacing": [
+            1
+        ],
+        "key-spacing": [
+            1
+        ],
+        "array-bracket-spacing": [
+            1
+        ],
+        "object-curly-spacing": [
+            1
+        ],
+        "spaced-comment": [
+            1,
+            "always",
+            {
+                "exceptions": [
+                    "-",
+                    "+",
+                    "*"
+                ]
+            }
+        ],
+        "for-direction": [
+            2
+        ],
+        "no-await-in-loop": [
+            2
+        ],
+        "no-prototype-builtins": [
+            2
+        ],
+        "default-case": [
+            2
+        ],
+        "eqeqeq": [
+            2
+        ],
+        "no-loop-func": [
+            2
+        ],
+        "no-unmodified-loop-condition": [
+            2
+        ],
+        "require-await": [
+            2
+        ],
+        "wrap-iife": [
+            1,
+            "inside"
+        ],
+        "semi": [
+            1,
+            "always"
+        ],
+        "guard-for-in": [
+            2
+        ],
+        "no-unused-vars": [
+            2,
+            {
+                "argsIgnorePattern": "^(require)|(exports)|(module)$"
+            }
+        ],
+        "no-alert": [
+            1
+        ],
+        "no-console": [
+            0
+        ],
+        "no-caller": [
+            2
+        ],
+        "no-eval": [
+            1
+        ],
+        "no-with": [
+            2
+        ],
+        "no-restricted-globals": [
+            2,
+            "event",
+            "undefined"
+        ],
+        "no-extend-native": [
+            1
+        ],
+        "no-new-wrappers": [
+            2
+        ],
+        "no-param-reassign": [
+            2
+        ],
+        "no-magic-numbers": [
+            1
+        ],
+        "no-throw-literal": [
+            2
+        ],
+        "prefer-promise-reject-errors": [
+            2
+        ],
+        "no-undef-init": [
+            2
+        ],
+        "vars-on-top": [
+            1
+        ],
+        "no-use-before-define": [
+            2
+        ],
+        "eol-last": [
+            2,
+            "always"
+        ],
+        "arrow-body-style": [
+            1
+        ],
+        "arrow-parens": [
+            1,
+            "as-needed"
+        ],
+        "new-cap": [
+            2
+        ],
+        "no-useless-escape": 0,
+        "no-control-regex": 0
+    }
+}
